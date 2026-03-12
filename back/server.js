2ed64d8f015fed159690425e7c3faeb19cee21c7
FifaRandria <fifarandria92@gmail.com>
2026-03-12 23:07:07 +0100

    install mongoose and try to configure databasein server.js

--- back/server.js
+++ back/server.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 
 const app = express();
@@ -7,11 +10,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => 
+{
+    console.log("MongoDB connected");
+})
+.catch((error)=>
+{
+    console.log("MongoDB connection error", error);
+});
+
 app.get("/", (req, res) =>
+
 {
     res.json({ message: "API working" });
 });
 
-app.listen(5000, () => {
-    console.log ("Server running on port 5000");
+app.listen(PORT, () => {
+    console.log (`Server running on port ${PORT}`);
 });
